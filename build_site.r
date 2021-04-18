b3671a5ffb3344e25ce2e3aa94463618de33bd9a
Sam S. Webb <55401545+SamSWebb@users.noreply.github.com>
2021-04-18 19:43:48 +0100

    Add files via upload

--- build_site.r
+++ build_site.r
@@ -2,7 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/samwebb/Documents/SamSWebb-master")
+setwd("/Users/samwebb/Documents/GitHub/SamSWebb.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
