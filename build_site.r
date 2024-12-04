05a4646bb974732c0ff24d971512a130f2642d40
Sam S. Webb <55401545+SamSWebb@users.noreply.github.com>
2024-12-04 12:36:35 +0000

    updated build website function
    to add participate page

--- build_site.r
+++ build_site.r
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/samwebb/Library/CloudStorage/OneDrive-Nexus365/SamSWebb.github.io-master")
+setwd("'/Users/samwebb/Library/CloudStorage/GoogleDrive-samswebb96@gmail.com/My Drive/SamSWebb.github.io-master'")
 
 #render your sweet site. 
 rmarkdown::render_site()
